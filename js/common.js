a1b0eb748b049e1917d7484b52c5ab057e2599fb
akella <akella.a@gmail.com>
2014-03-28 23:56:06 +0200

    site done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
+	$('.header__gotocards').click(function(event) {
+		var pos = $('.cards').offset();
+		$('body').animate({ scrollTop: pos.top },800);
+		e.preventDefault();
+	});
+	$('.scrolltotop').click(function(event) {
+		$('body').animate({ scrollTop: 0 },800);
+		e.preventDefault();
+	});
+	var windowheight = $(window).height();
+	$(window).scroll(function() {
 
+    var height = $(window).scrollTop();
+
+    if(height  > windowheight) {
+        $('body').addClass('is-bottom');
+    }
+    else{
+    	$('body').removeClass('is-bottom');
+    }
+});
 });
